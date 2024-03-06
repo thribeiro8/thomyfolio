0094f03d5847394807662f8744e5a403456112ff
Thomas Ribeiro <thomas18rs@gmail.com>
2024-03-06 18:28:13 -0300

    fix: Master component UI

--- src/pages/Home/sections/Master/Master.tsx
+++ src/pages/Home/sections/Master/Master.tsx
@@ -12,10 +12,16 @@ const Master = () => {
     height: "100vh",
     display: "flex",
     alignItems: "center",
+    [theme.breakpoints.up("xs")]: { // <= mobile
+      paddingTop: "100px"
+    },
+    [theme.breakpoints.up("md")]: { // >= mobile
+      paddingTop: "0"
+    },
   }));
 
   const StyledImage = styled("img")(() => ({
-    width: "80%",
+    width: "75%",
     borderRadius: "50%",
     border: `1px solid ${theme.palette.primary.contrastText}`,
   }));
